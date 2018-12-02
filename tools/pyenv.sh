54eaa74a1b0bfe9dda9113a6cafc2dca5674bb3c
Joe Kaufeld <joe.kaufeld@gmail.com>
2018-12-02 00:16:54 -0500

    add pyenv dependencies

--- tools/pyenv.sh
+++ tools/pyenv.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev
+
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
